6905fd3bf432607907eeda777c96f99634dea81a
InesM95 <inesmiled9@gmail.com>
2021-12-17 17:29:11 +0100

    Add bootstrapping page

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title> Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/nav.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+    <!-- NavBar -->
+    <header class="header" id="header">
+        <button class="btn" type="button">
+            <a href="src/login-user.php">
+                Login
+            </a>
+        </button>
+    </header>
+    <div class="l-navbar">
+        <nav class="nav">
+            <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">Chat App</span> </a>
+            <div class="nav_list">
+
+        </nav>
+    </div>
+    <!-- end NavBar -->
+
+
+    <div id="main">
+
+    </div>
